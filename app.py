a0c602ecfd3e1a8f4e7dc074ac6675a6c7ef0425
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-11 21:31:19 -0300

    feat: first setup

---
+++ app.py
@@ -0,0 +1,12 @@
+from http import HTTPStatus
+
+from fastapi import FastAPI
+
+from core.models.schemas import Message
+
+app = FastAPI()
+
+
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
+def read_root():
+    return {'message': 'Olá Mundo!'}
