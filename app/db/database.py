ae7c6d714949e20414b697eb0aa4fac296415214
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-21 16:51:34 -0300

    feat: add logger to info database connection

--- app/db/database.py
+++ app/db/database.py
@@ -1,9 +1,18 @@
+from loguru import logger
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 from app.core.settings import Settings
 
 engine = create_engine(Settings().DATABASE_URL)
+
+try:
+    with engine.connect() as connection:
+        logger.info(
+            f"Successfully connected to {Settings().DATABASE_TYPE} database."
+        )
+except Exception as e:
+    logger.error(f"Failed to connect to the database: {e}")
 
 
 def get_session():
