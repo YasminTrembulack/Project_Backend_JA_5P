a4973d42fe3820fb1cc065f8d14394f16f5967f0
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-21 21:48:07 -0300

    feat: create migration in code

--- app/db/database.py
+++ app/db/database.py
@@ -1,20 +1,44 @@
+from alembic import command
+from alembic.config import Config
 from loguru import logger
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, sessionmaker
 
 from app.core.settings import Settings
 
 engine = create_engine(Settings().DATABASE_URL)
 
-try:
-    with engine.connect() as connection:
-        logger.info(
-            f"Successfully connected to {Settings().DATABASE_TYPE} database."
-        )
-except Exception as e:
-    logger.error(f"Failed to connect to the database: {e}")
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def test_connection():
+    try:
+        with engine.connect():
+            logger.info(
+                f"Successfully connected to {Settings().DATABASE_TYPE} database."
+            )
+    except Exception as e:
+        logger.error(f"Failed to connect to the database: {e}")
+        raise e
+    finally:
+        engine.dispose()
+
+
+def run_migrations():
+    try:
+        alembic_cfg = Config("alembic.ini")
+        with engine.connect() as connection:
+            alembic_cfg.attributes['connection'] = connection
+            logger.info("Executando migrations...")
+            command.upgrade(alembic_cfg, "head")
+            logger.info("Migrations executadas com sucesso!")
+    except Exception as e:
+        logger.error(f"Error during migrations execution: {e}")
+        raise
+    finally:
+        engine.dispose()
 
 
 def get_session():
-    with Session(engine) as session:
+    with SessionLocal() as session:
         yield session
