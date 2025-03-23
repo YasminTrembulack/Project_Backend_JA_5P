61cf191ad09ef96fe26237a308815af73cb7b0c3
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-23 20:07:27 -0300

    feat: update ping route

--- app/routes/ping.py
+++ app/routes/ping.py
@@ -1,5 +1,6 @@
-import datetime
+from datetime import datetime, timezone
 
+import pytz
 from fastapi import APIRouter, status
 
 from app.core.settings import Settings
@@ -10,8 +11,11 @@ router = APIRouter()
 
 @router.get('/ping', status_code=status.HTTP_200_OK, response_model=PingResponse)
 def read_root():
+    br_tz = pytz.timezone("America/Sao_Paulo")
+    timestamp = datetime.now(timezone.utc).isoformat()
     return {
         'project_name': Settings().PROJECT_NAME,
         'version': Settings().VERSION,
-        'timestamp': datetime.utcnow().isoformat()
+        'timestamp_br': datetime.fromisoformat(timestamp)
+        .astimezone(br_tz).strftime("%d/%m/%Y %H:%M:%S")
     }
