cac1e4b6405274edb5646a4f4a43fcd296dff5a6
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-23 19:51:45 -0300

    feat: add excptions to authentication middleware

--- app/middleware/authentication.py
+++ app/middleware/authentication.py
@@ -1,11 +1,18 @@
 
+from fastapi import status
 from fastapi.responses import JSONResponse
+from loguru import logger
+from sqlalchemy.orm import Session
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from app.core.security import security
 from app.db.database import get_session
 from app.repositories.user_repositorie import UserRepository
-from app.types.exceptions import APIException
+from app.types.exceptions import (
+    APIException,
+    AuthTokenMissingError,
+    InvalidTokenError,
+)
 
 
 class AuthenticationMiddleware(BaseHTTPMiddleware):
@@ -13,37 +20,37 @@ class AuthenticationMiddleware(BaseHTTPMiddleware):
     async def dispatch(request, call_next):
         if request.url.path.startswith('/api/login'):
             return await call_next(request)
-
-        auth_header = request.headers.get('Authorization')
-        if not auth_header or not auth_header.startswith('Bearer '):
-            return JSONResponse(
-                status_code=401,
-                content={"detail": "Authentication token is missing"}
-            )
-
-        token = auth_header.split(" ")[1].strip()
-
         try:
+            auth_header = request.headers.get('Authorization')
+            if not auth_header or not auth_header.startswith('Bearer '):
+                raise AuthTokenMissingError("Authentication token is missing")
+
+            token = auth_header.split(' ')[1].strip()
+
             payload = security.verify_access_token(token)
-            user_id = payload.get("user_id")
+            user_id = payload.get('user_id')
+
+            session: Session = next(get_session())
+            try:
+                repo = UserRepository(session)
+                user = repo.get_user_by_id(user_id)
+            finally:
+                session.close()
+
+            if user is None:
+                raise InvalidTokenError()
+            request.state.user = user
+
         except APIException as e:
+            logger.error(f'{e.__class__.__name__}: {e.message}')
             return JSONResponse(
-                status_code=401,
-                content={"detail": str(e)}
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content={'detail': f'{e.message}'}
             )
-        except Exception:
+        except Exception as e:
+            logger.error(f'{e.__class__.__name__}: {e.message}')
             return JSONResponse(
-                status_code=401,
-                content={"detail": "Unexpected error while verifying token."}
-            )
-
-        with next(get_session()) as db:
-            repo = UserRepository(db)
-            user = repo.get_user_by_id(user_id)
-
-        if user is None:
-            return JSONResponse(
-                status_code=404,
-                content={"detail": 'Invalid access token'}
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                content={'detail': 'Unexpected error while verifying token.'}
             )
         return await call_next(request)
