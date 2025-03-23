8872f3b591e721f93b8d0e8857ff581eff7d1aee
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-23 19:53:40 -0300

    feat: update create_exception_handler

--- app/middleware/erro_handling.py
+++ app/middleware/erro_handling.py
@@ -4,34 +4,21 @@ from fastapi import Request
 from fastapi.responses import JSONResponse
 from loguru import logger
 
-from app.types.exceptions import (
-    APIException,
-)
+from app.types.exceptions import APIException
 
 
 def create_exception_handler(
     status_code: int, initial_detail: str
 ) -> Callable[[Request, APIException], JSONResponse]:
-    detail = {"message": initial_detail}
+    detail = {'message': initial_detail}
 
     async def exception_handler(_: Request, exc: APIException) -> JSONResponse:
         if exc.message:
-            detail["message"] = exc.message
+            detail['message'] = exc.message
 
-        if exc.name:
-            detail["message"] = f"{detail['message']} [{exc.name}]"
-
-        logger.error(exc)
+        logger.error(f'{exc.__class__.__name__}: {exc.message}')
         return JSONResponse(
-            status_code=status_code, content={"detail": detail["message"]}
+            status_code=status_code, content={'detail': detail['message']}
         )
 
     return exception_handler
-
-
-# app.add_exception_handler(
-#     exc_class_or_status_code=ExpiredSignatureError,
-#     handler=create_exception_handler(
-#         status.HTTP_401_UNAUTHORIZED, "Token has expired. Please log in again."
-#     ),
-# )
