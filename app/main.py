d14443656186395850cd1c0c3febb7d01f21de4a
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-24 19:57:13 -0300

    feat: update app main

--- app/main.py
+++ app/main.py
@@ -5,8 +5,8 @@ from starlette.middleware.cors import CORSMiddleware
 
 from app.core.settings import Settings
 from app.db.database import run_migrations, test_connection
-from app.middleware.authentication import AuthenticationMiddleware
-from app.middleware.erro_handling import create_exception_handler
+from app.middlewares.authentication import AuthenticationMiddleware
+from app.middlewares.erro_handling import create_exception_handler
 from app.routes.auth_route import router as auth_router
 from app.routes.ping import router as ping_route
 from app.routes.user_route import router as user_router
@@ -28,18 +28,21 @@ async def lifespan(_: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-app.include_router(user_router, prefix=Settings().API_PREFIX)
-app.include_router(ping_route, prefix=Settings().API_PREFIX)
-app.include_router(auth_router, prefix=Settings().API_PREFIX)
 
-app.add_middleware(AuthenticationMiddleware)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*'],
+    allow_methods=['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allow_headers=['Authorization', 'Content-Type'],
 )
+
+app.add_middleware(AuthenticationMiddleware)
+
+app.include_router(user_router, prefix=Settings().API_PREFIX)
+app.include_router(ping_route, prefix=Settings().API_PREFIX)
+app.include_router(auth_router, prefix=Settings().API_PREFIX)
+
 
 app.add_exception_handler(
     exc_class_or_status_code=PermissionDeniedError,
