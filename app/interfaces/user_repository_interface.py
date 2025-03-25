408b7282af49091ca7ddbf9291f9ce274a21f656
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-25 14:02:16 -0300

    feat: update user repository interface

--- app/interfaces/user_repository_interface.py
+++ app/interfaces/user_repository_interface.py
@@ -1,4 +1,7 @@
 from abc import ABC, abstractmethod
+from typing import List, Optional, Tuple
+
+from sqlalchemy import UnaryExpression
 
 from app.models.user import User
 from app.types.schemas import UserPayload
@@ -10,9 +13,11 @@ class IUserRepository(ABC):
         pass
 
     @abstractmethod
-    def get_user_by_email(self, email: str) -> User | None:
+    def get_user_by_field(self, field_name: str, value: str) -> Optional[User]:
         pass
 
     @abstractmethod
-    def get_user_by_id(self, user_id: str) -> User | None:
+    def get_all_users_paginated(
+        self, offset: int, limit: int, order: UnaryExpression
+    ) -> Tuple[List[User], int]:
         pass
