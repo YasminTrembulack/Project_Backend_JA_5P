fdd544285799bba18f0f79fe89d275054b227e45
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-26 13:02:18 -0300

    feat: update user service

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -15,16 +15,7 @@ class UserService:
         self.user_repo = UserRepository(db)
 
     def user_register(self, user: UserPayload) -> User:
-        user_found = self.user_repo.get_user_by_field('email', user.email)
-        user_found = self.user_repo.get_user_by_field(
-            'registration_number', user.registration_number
-        )
-        if user_found:
-            raise DataConflictError(
-                'Email alredy in use.'
-                if user.email == user_found.email
-                else 'Registration number alredy in use.'
-            )
+        self._validate_unique_fields(user)
         user.password = security.hash_password(user.password)
         return self.user_repo.create_user(user)
 
@@ -57,25 +48,25 @@ class UserService:
 
         return self.user_repo.update_user(user, payload)
 
-    def _validate_unique_fields(self, payload: dict, user_id: str):
+    def get_user(self, id: str) -> User:
+        user = self.user_repo.get_user_by_field('id', id)
+        if not user:
+            raise NotFoundError('User not found')
+        return user
+
+    def _validate_unique_fields(self, payload: dict, user_id: str = None):
         if 'email' in payload and self._is_field_in_use(
             'email', payload['email'], user_id
         ):
             raise DataConflictError('Email already in use.')
 
         if 'registration_number' in payload and self._is_field_in_use(
-            'registration_number',
-            payload['registration_number'],
-            user_id,
+            'registration_number', payload['registration_number'], user_id,
         ):
             raise DataConflictError('Registration number already in use.')
 
     def _is_field_in_use(self, field: str, value: str, user_id: str) -> bool:
         existing_user = self.user_repo.get_user_by_field(field, value)
-        return existing_user is not None and existing_user.id != user_id
-
-    def get_user(self, id: str) -> User:
-        user = self.user_repo.get_user_by_field('id', id)
-        if not user:
-            raise NotFoundError('User not found')
-        return user
+        if user_id:
+            return existing_user is not None and existing_user.id != user_id
+        return existing_user is not None
