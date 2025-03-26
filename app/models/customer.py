a28fbc0de7fd4f3508aa4d2dab1297a96d456f52
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-26 12:16:51 -0300

    feat: add unique to customer full_name and create new migration

--- app/models/customer.py
+++ app/models/customer.py
@@ -14,7 +14,7 @@ class Customer(BaseModel):
     __tablename__ = 'customers'
 
     id: Mapped[UUID] = mapped_column(CHAR(36), primary_key=True, default=uuid4)
-    full_name: Mapped[str] = mapped_column(String(255))
+    full_name: Mapped[str] = mapped_column(String(255), unique=True)
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         server_default=func.now(), onupdate=func.now()
