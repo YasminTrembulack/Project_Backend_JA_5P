5fd3021cee5c74aaf970759b8d7460dd85fe4bbe
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-26 20:03:34 -0300

    feat: add new atributes to customer table and make a new migration

--- app/models/customer.py
+++ app/models/customer.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from datetime import datetime
+from enum import Enum
 from uuid import UUID, uuid4
 
 from sqlalchemy import String, func
@@ -15,7 +16,36 @@ class Customer(BaseModel):
 
     id: Mapped[UUID] = mapped_column(CHAR(36), primary_key=True, default=uuid4)
     full_name: Mapped[str] = mapped_column(String(255), unique=True)
+    country_code: Mapped[str] = mapped_column(String(2))
+    country_name: Mapped[str] = mapped_column(String(50))
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         server_default=func.now(), onupdate=func.now()
     )
+
+
+class CountryEnum(str, Enum):    
+    CN = "China"
+    US = "United States"
+    JP = "Japan"
+    DE = "Germany"
+    KR = "South Korea"
+    IN = "India"
+    FR = "France"
+    IT = "Italy"
+    GB = "United Kingdom"
+    ES = "Spain"
+    MX = "Mexico"
+    BR = "Brazil"
+    TH = "Thailand"
+    RU = "Russia"
+    CZ = "Czech Republic"
+    TR = "Turkey"
+
+    @classmethod
+    def get_country_code(cls, name: str) -> str:
+        name_lower = name.lower()
+        for code, country in cls.__members__.items():
+            if country.value.lower() == name_lower:
+                return code
+        raise ValueError(f"Country not found: {name}")
