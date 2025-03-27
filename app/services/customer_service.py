bbf626eb21fdab64cc54422fce3e8c93c8ef2efd
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-27 14:09:14 -0300

    feat: update customer service

--- app/services/customer_service.py
+++ app/services/customer_service.py
@@ -1,29 +1,34 @@
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 from sqlalchemy import desc
 from sqlalchemy.orm import Session
 
-from app.models.customer import CountryEnum, Customer
+from app.models.customer import Customer
 from app.repositories.customer_repositorie import CustomerRepository
-from app.types.exceptions import (
-    DataConflictError,
-    InvalidCountryError,
-    NotFoundError,
-)
-from app.types.schemas import CustomerPayload, CustomerUpdatePayload
+from app.types.exceptions import DataConflictError, InvalidFieldError, NotFoundError
+from app.types.schemas import CustomerBase, CustomerPayload, CustomerUpdatePayload
 
 
 class CustomerService:
     def __init__(self, db: Session):
         self.customer_repo = CustomerRepository(db)
 
-    def customer_register(self, customer: CustomerPayload) -> Customer:
-        self._validate_unique_fields(customer.model_dump())
-        return self.customer_repo.create_customer(customer)
+    def customer_register(self, payload: CustomerPayload) -> Customer:
+        inactive_duplicate = self._get_or_validate_customer_uniqueness(
+            payload.full_name, payload.country_name
+        )
+        if not inactive_duplicate:
+            return self.customer_repo.create_customer(payload)
+        update_customer = self._update_customer_fields(payload, inactive_duplicate)
+        return self.customer_repo.restore_customer(update_customer)
 
     def get_all_customers(
         self, page: int, limit: int, order_by: str, desc_order: bool
     ) -> Tuple[List[Customer], int]:
+        if not hasattr(Customer, order_by):
+            raise InvalidFieldError(
+                f'Field {order_by} does not exist on Customer model'
+            )
         offset = (page - 1) * limit
         order = (
             desc(getattr(Customer, order_by))
@@ -33,45 +38,53 @@ class CustomerService:
         return self.customer_repo.get_all_customers_paginated(offset, limit, order)
 
     def delete_customer(self, id: str) -> None:
-        customer = self.customer_repo.get_customer_by_field('id', id)
-        if not customer:
-            raise NotFoundError('Customer not found')
+        customer = self._get_customer_or_404(id)
+
         return self.customer_repo.delete_customer(customer)
 
     def update_customer(self, id: str, payload: CustomerUpdatePayload) -> Customer:
-        customer = self.customer_repo.get_user_by_field('id', id)
-        if not customer:
-            raise NotFoundError('Customer not found')
+        customer = self._get_customer_or_404(id)
+        updated_data = payload.model_dump(exclude_unset=True)
+        
+        new_full_name = updated_data.get("full_name", customer.full_name)
+        new_country_name = updated_data.get("country_name", customer.country_name)
 
-        self._validate_unique_fields(payload.model_dump(), customer.id)
-        payload = payload.model_dump(exclude_unset=True)
-        if payload.get('country_name'):
-            payload['country_code'] = CountryEnum.get_country_code(
-                payload['country_name']
-            )
-        return self.customer_repo.update_customer(customer, payload)
+        inactive_duplicate = self._get_or_validate_customer_uniqueness(
+            new_full_name, new_country_name, customer.id
+        )
+        
+        if inactive_duplicate:
+            self.customer_repo.delete_customer(customer)
+            return self.customer_repo.restore_customer(inactive_duplicate)
+
+        updated_customer = self._update_customer_fields(payload, customer)
+        return self.customer_repo.update_customer(updated_customer)
 
     def get_customer(self, id: str) -> Customer:
+        return self._get_customer_or_404(id)
+
+    def _get_or_validate_customer_uniqueness(
+        self, full_name: str, country_name: str, customer_id: str = None
+    ) -> Customer | None:
+        if (customer := self.customer_repo.exists_by_fullname_and_country(
+            full_name, country_name, exclude_id=customer_id, include_inactive=True
+        )):
+            if customer.is_active:
+                raise DataConflictError(
+                    f"A customer with name '{full_name}' in country '{country_name}' already exists"
+                )
+            return customer
+        return None
+
+    def _get_customer_or_404(self, id: str) -> Customer:
         customer = self.customer_repo.get_customer_by_field('id', id)
         if not customer:
             raise NotFoundError('Customer not found')
         return customer
-
-    def _validate_unique_fields(self, payload: dict, customer_id: str = None):
-        if payload.get('full_name', False) and self._is_field_in_use(
-            'full_name', payload.get('full_name'), customer_id
-        ):
-            raise DataConflictError('Full name already in use.')
-
-        if payload.get('country_name', False):
-            valid_names = [country.value for country in CountryEnum]
-            if payload.get('country_name') not in valid_names:
-                raise InvalidCountryError(
-                    f"The country '{payload.get('country_name')}' is not supported."
-                )
-
-    def _is_field_in_use(self, field: str, value: str, customer_id: str) -> bool:
-        existing_customer = self.customer_repo.get_customer_by_field(field, value)
-        if not customer_id:
-            return existing_customer is not None
-        return existing_customer is not None and existing_customer.id != customer_id
+    
+    @staticmethod
+    def _update_customer_fields(payload: CustomerBase, target: Customer) -> Customer:
+        for key, value in payload.model_dump(exclude_unset=True).items():
+            if hasattr(target, key) and value is not None:
+                setattr(target, key, value)
+        return target
