6d296a8aa41c01055ffcd0a9bcaf3708d3ba62cb
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-27 20:43:47 -0300

    feat: update user service

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from typing import List, Tuple
 
 from sqlalchemy import desc
@@ -15,23 +16,15 @@ class UserService:
         self.user_repo = UserRepository(db)
 
     def user_register(self, payload: UserPayload) -> User:
-        inactive_duplicate = self._get_or_validate_user_uniqueness(
-            payload.email, payload.registration_number
-        )
+        self._validate_user_uniqueness(payload.email, payload.registration_number)
         payload.password = security.hash_password(payload.password)
-        if not inactive_duplicate:
-            return self.user_repo.create_user(payload)
-        update_user = self._update_user_fields(payload, inactive_duplicate)
-        return self.user_repo.restore_user(update_user)
-        
+        return self.user_repo.create_user(payload)
 
     def get_all_users(
         self, page: int, limit: int, order_by: str, desc_order: bool
     ) -> Tuple[List[User], int]:
         if not hasattr(User, order_by):
-            raise InvalidFieldError(
-                f'Field {order_by} does not exist on User model'
-            )
+            raise InvalidFieldError(f'Field {order_by} does not exist on User model')
         offset = (page - 1) * limit
         order = (
             desc(getattr(User, order_by)) if desc_order else getattr(User, order_by)
@@ -40,6 +33,9 @@ class UserService:
 
     def delete_user(self, id: str) -> None:
         user = self._get_user_or_404(id)
+        timestamp = int(datetime.now(timezone.utc).timestamp())
+        user.email = f'deleted_{timestamp}_{user.email}'
+        user.registration_number = f'deleted_{timestamp}_{user.registration_number}'
         return self.user_repo.delete_user(user)
 
     def update_user(self, id: str, payload: UserUpdatePayload) -> User:
@@ -48,72 +44,42 @@ class UserService:
 
         new_email = updated_data.get('email', user.full_name)
         new_registration_number = updated_data.get(
-            'registration_number', user.registration_number)
-        
-        inactive_duplicate = self._get_or_validate_user_uniqueness(
-            new_email, new_registration_number, user.id
+            'registration_number', user.registration_number
         )
+
+        if new_email or new_registration_number:
+            self._validate_user_uniqueness(
+                new_email, new_registration_number, user.id
+            )
 
         if payload.password:
             payload.password = security.hash_password(payload.password)
-        
-        if inactive_duplicate:
-            self.user_repo.delete_user(user)
-            return self.user_repo.restore_user(inactive_duplicate)
-        
+
         updated_user = self._update_user_fields(payload, user)
         return self.user_repo.update_user(updated_user)
 
     def get_user(self, id: str) -> User:
         return self._get_user_or_404(id)
 
-    def _is_field_in_use(self, field: str, value: str, user_id: str) -> bool:
-        existing_user = self.user_repo.get_user_by_field(field, value)
-        if user_id:
-            return existing_user is not None and existing_user.id != user_id
-        return existing_user is not None
-
-    def _get_or_validate_user_uniqueness(
-        self, email: str, registration_number: str, exclude_id: str = None
-    ) -> User | None:
-        user_with_email = self.user_repo.get_user_by_field(
-            'email', email, exclude_id=exclude_id, include_inactive=True
-        )
-        user_with_rn = self.user_repo.get_user_by_field(
-            'registration_number',
-            registration_number,
-            exclude_id=exclude_id,
-            include_inactive=True
-        )
-
-        if user_with_email and user_with_email.is_active:
+    def _validate_user_uniqueness(
+        self, email: str, reg_number: str, exclude_id: str = None
+    ) -> None:
+        if self.user_repo.get_user_by_field('email', email, exclude_id):
             raise DataConflictError(f"A user with email '{email}' already exists.")
-        
-        if user_with_rn and user_with_rn.is_active:
-            raise DataConflictError(
-                f"A user with registration number \
-                '{registration_number}' already exists."
-            )
-        
-        if user_with_email and not user_with_email.is_active:
-            if user_with_rn and not user_with_rn.is_active:
-                if user_with_email.id != user_with_rn.id:
-                    raise DataConflictError(
-                        f"Both email '{email}' and registration number \
-                        '{registration_number}' are in use by different inactive \
-                        customers."
-                    )
-                return user_with_email
 
-        if user_with_rn and not user_with_rn.is_active:
-            return user_with_rn 
-        
+        if self.user_repo.get_user_by_field(
+            'registration_number', reg_number, exclude_id
+        ):
+            raise DataConflictError(
+                f"A user with registration number '{reg_number}' already exists."
+            )
+
     def _get_user_or_404(self, id: str) -> User:
         user = self.user_repo.get_user_by_field('id', id)
         if not user:
             raise NotFoundError('User not found')
         return user
-    
+
     @staticmethod
     def _update_user_fields(payload: UserBase, target: User) -> User:
         for key, value in payload.model_dump(exclude_unset=True).items():
