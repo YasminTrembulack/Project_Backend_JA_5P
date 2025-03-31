3811f57877efca97e0085a8e9c33ae996fba70c8
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-03-31 09:59:41 -0300

    fix: conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
-from dotenv import load_dotenv
-
-from app.db.database import import_models
-
+from dotenv import load_dotenv  # noqa: I001
 load_dotenv()
+
+from app.db.database import import_models  # noqa: E402, I001
 import_models()
