2dfeb79bca281e72558512db05378b6975ec78d7
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-05-08 15:04:54 -0300

    feat: update update_part_progress

--- app/repositories/part_repositorie.py
+++ app/repositories/part_repositorie.py
@@ -6,9 +6,16 @@ from sqlalchemy.orm import Session
 
 from app.interfaces.part_repository_interface import IPartRepository
 from app.models.part import Part
-from app.types.enums import MaterialStatusEnum, OpStatusEnum
+from app.types.enums import (
+    MaterialStatusEnum,
+    OpStatusEnum,
+    PartStatusEnum,
+    SimpleStatusEnum,
+)
 from app.types.exceptions import InvalidFieldError
 from app.types.schemas import PartPayload
+
+COMPLETED_PERCENTAGE = 100
 
 
 class PartRepository(IPartRepository):
@@ -104,14 +111,26 @@ class PartRepository(IPartRepository):
             oa for oa in part.operation_associations if oa.is_active
         ]
 
-        total = len(active_operations) + len(active_material_associations)
-        done = sum(
-            1 for oa in active_operations if oa.status == OpStatusEnum.COMPLETED
-        ) + sum(
-            1
-            for m in active_material_associations
-            if m.status == MaterialStatusEnum.AVAILABLE
+        total = len(active_operations) + len(active_material_associations) + 2
+        done = (
+            sum(1 for oa in active_operations if oa.status == OpStatusEnum.COMPLETED)
+            + sum(
+                1
+                for m in active_material_associations
+                if m.status == MaterialStatusEnum.AVAILABLE
+            )
+            + (1 if part.model_3d == SimpleStatusEnum.APPROVED else 0)
+            + (1 if part.nc_program == SimpleStatusEnum.APPROVED else 0)
+        )
+        pp = int((done / total) * 100) if total > 0 else 0
+        part.progress_percentage = pp
+
+        part.status = (
+            PartStatusEnum.COMPLETED
+            if pp == COMPLETED_PERCENTAGE
+            else PartStatusEnum.IN_PROGRESS
+            if pp > 0
+            else PartStatusEnum.PENDING
         )
 
-        part.progress_percentage = int((done / total) * 100) if total > 0 else 0
         self.db.commit()
