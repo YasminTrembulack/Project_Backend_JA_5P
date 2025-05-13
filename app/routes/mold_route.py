a8748e4a2911ea8da284ad51b58d7f2f7242270d
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-05-13 10:19:35 -0300

    feat: implemement mold service and route

--- app/routes/mold_route.py
+++ app/routes/mold_route.py
@@ -1,13 +1,17 @@
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Query, status
 from sqlalchemy.orm import Session
 
 from app.db.database import get_session
 from app.middlewares.check_roles import check_roles
 from app.services.mold_service import MoldService
 from app.types.schemas import (
+    DeleteResponse,
     EntityResponse,
+    GetAllResponse,
+    Metadata,
     MoldPayload,
     MoldResponde,
+    MoldUpdatePayload,
 )
 
 router = APIRouter(prefix='/mold')
@@ -30,3 +34,84 @@ def create_mold(
 
     mold_response = MoldResponde.model_validate(db_mold.to_dict())
     return EntityResponse(message='Mold created with success.', data=mold_response)
+
+
+
+@router.get(
+    '/all',
+    status_code=status.HTTP_200_OK,
+    response_model=GetAllResponse[MoldResponde],
+)
+def get_all_molds(
+    page: int = Query(1, ge=1),
+    limit: int = Query(10, ge=1, le=50),
+    order_by: str = Query('created_at'),
+    desc_order: bool = Query(False),
+    session: Session = Depends(get_session),
+    _: None = Depends(check_roles(['Admin', 'User', 'Editor'])),
+):
+    service = MoldService(session)
+    molds, total_molds = service.get_all_molds(page, limit, order_by, desc_order)
+    total_pages = (total_molds + limit - 1) // limit
+    meta = Metadata(
+        total=total_molds,
+        limit=limit,
+        page=page,
+        total_pages=total_pages,
+        has_next=page < total_pages,
+        has_previous=page > 1,
+        order_by=order_by,
+        desc_order=desc_order,
+    )
+    molds = [MoldResponde.model_validate(m.to_dict()) for m in molds]
+    return GetAllResponse(
+        message='Molds found successfully.', data=molds, metadata=meta
+    )
+
+
+@router.delete(
+    '/delete/{id}',
+    status_code=status.HTTP_200_OK,
+    response_model=DeleteResponse,
+)
+def delete_mold(
+    id: str,
+    session: Session = Depends(get_session),
+    _: None = Depends(check_roles(['Admin'])),
+):
+    service = MoldService(session)
+    service.delete_mold(id)
+    return DeleteResponse(message='Mold deleted successfully.')
+
+
+@router.get(
+    '/{id}',
+    status_code=status.HTTP_200_OK,
+    response_model=EntityResponse[MoldResponde],
+)
+def get_mold(
+    id: str,
+    session: Session = Depends(get_session),
+    _: None = Depends(check_roles(['Admin', 'User', 'Editor'])),
+):
+    service = MoldService(session)
+    mold = service.get_mold(id)
+    mold_response = MoldResponde.model_validate(mold.to_dict())
+    return EntityResponse(message='Mold found successfully.', data=mold_response)
+
+
+@router.patch(
+    '/update/{id}',
+    status_code=status.HTTP_200_OK,
+    response_model=EntityResponse[MoldResponde],
+)
+def update_mold(
+    id: str,
+    mold: MoldUpdatePayload,
+    session: Session = Depends(get_session),
+    _: None = Depends(check_roles(['Admin', 'Editor'])),
+):
+    service = MoldService(session)
+    mold = service.update_mold(id, mold)
+    mold_response = MoldResponde.model_validate(mold.to_dict())
+    return EntityResponse(message='Mold updated successfully.', data=mold_response)
--- app/services/mold_service.py
+++ app/services/mold_service.py
@@ -1,27 +1,28 @@
+from datetime import date
+from typing import List, Tuple
+
+from sqlalchemy import desc
 from sqlalchemy.orm import Session
+from sqlalchemy.orm.attributes import InstrumentedAttribute
 
 from app.models.customer import Customer
 from app.models.mold import Mold
 from app.repositories.customer_repositorie import CustomerRepository
 from app.repositories.mold_repositorie import MoldRepository
-from app.types.exceptions import DataConflictError, NotFoundError
-from app.types.schemas import MoldPayload
-
-URGENT_DAYS_THRESHOLD = 5
-HIGH_DAYS_THRESHOLD = 10
-MEDIUM_DAYS_THRESHOLD = 20
-
-URGENT_PRIORITY_THRESHOLD = 6
-HIGH_PRIORITY_THRESHOLD = 4
-MEDIUM_PRIORITY_THRESHOLD = 2
+from app.repositories.part_repositorie import PartRepository
+from app.services.progress_service import ProgressService
+from app.types.exceptions import DataConflictError, InvalidFieldError, NotFoundError
+from app.types.schemas import MoldBase, MoldPayload, MoldUpdatePayload
 
 
 class MoldService:
     def __init__(self, db: Session):
-        self.mold_repo = MoldRepository(db)
         self.customer_repo = CustomerRepository(db)
+        self.mold_repo = MoldRepository(db)
+        self.part_repo = PartRepository(db)
+        self.progress_service = ProgressService(self.mold_repo, self.part_repo)
 
-    def mold_register(self, payload: MoldPayload) -> Mold:  # ! OK
+    def mold_register(self, payload: MoldPayload) -> Mold:
         self._get_customer_or_404(payload.customer_id)
         if payload.name:
             self._validate_name_uniqueness(payload.name)
@@ -30,106 +31,82 @@ class MoldService:
             payload.name = str(new_name)
         return self.mold_repo.create_mold(payload)
 
-    # def _get_all_molds(
-    #     self, page: int, limit: int, order_by: str, desc_order: bool
-    # ) -> Tuple[List[Mold], int]:
-    #     order_attr = getattr(Mold, order_by, None)
+    def get_all_molds(
+        self, page: int, limit: int, order_by: str, desc_order: bool
+    ) -> Tuple[List[Mold], int]:
+        order_attr = getattr(Mold, order_by, None)
 
-    #     if not isinstance(order_attr, InstrumentedAttribute):
-    #         raise InvalidFieldError(
-    #             f'Field {order_by} does not exist or is not sortable.'
-    #         )
-    #     offset = (page - 1) * limit
-    #     order = (
-    #         desc(getattr(Mold, order_by))
-    #         if desc_order else getattr(Part, order_by)
-    #     )
-    #     return self.part_repo.get_all_parts_paginated(offset, limit, order)
+        if not isinstance(order_attr, InstrumentedAttribute):
+            raise InvalidFieldError(
+                f'Field {order_by} does not exist or is not sortable.'
+            )
+        offset = (page - 1) * limit
+        
+        order_attr = getattr(Mold, order_by)
+        order = desc(order_attr) if desc_order else order_attr
 
-    # def delete_mold(self, id: str) -> None:
-    #     mold = self._get_mold_or_404(id)
-    #     self.mold_repo.delete_mold(mold)
-
-    # def update_mold(self, id: str, payload: MoldUpdatePayload) -> Mold:
-    #     mold = self._get_mold_or_404(id)
-
-    #     if payload.priority is not None:
-    #         raise InvalidFieldError('Priority cannot be changed')
-
-    #     updated_mold = self._update_mold_fields(payload, mold)
-    #     return self.mold_repo.update_mold(updated_mold)
-
-    # def get_mold(self, id: str) -> Mold:
-    #     return self._get_mold_or_404(id)
-
+        molds, total_molds = self.mold_repo.get_all_molds_paginated(offset, limit, order)
+        return [self._calculate_priority(m) for m in molds], total_molds
+    
     def _get_customer_or_404(self, id: str) -> Customer:
         customer = self.customer_repo.get_customer_by_field('id', id)
         if not customer:
             raise NotFoundError('Customer not found')
         return customer
-
-    # def _get_mold_or_404(self, id: str) -> Mold:
-    #     mold = self.mold_repo.get_mold_by_field('id', id)
-    #     if not mold:
-    #         raise NotFoundError('Mold not found')
-    #     return mold
-
-    # def _calculate_priority(
-    #     self, delivery_date: datetime, parts: List[Part]
-    # ) -> PriorityEnum:
-    #     now = datetime.now()
-    #     days_until_delivery = (delivery_date - now).days
-
-    #     if days_until_delivery <= URGENT_DAYS_THRESHOLD:
-    #         priority_score = 4  # Urgente
-    #     elif days_until_delivery <= HIGH_DAYS_THRESHOLD:
-    #         priority_score = 3  # Alta
-    #     elif days_until_delivery <= MEDIUM_DAYS_THRESHOLD:
-    #         priority_score = 2  # Média
-    #     else:
-    #         priority_score = 1  # Baixa
-
-    #     parts_score = self.__sum_parts_progress(parts)
-
-    #     parts_not_ready = len(parts) - parts_score
-    #     priority_score += parts_not_ready
-
-    #     if priority_score >= URGENT_PRIORITY_THRESHOLD:
-    #         return PriorityEnum.URGENT
-    #     elif priority_score >= HIGH_PRIORITY_THRESHOLD:
-    #         return PriorityEnum.HIGH
-    #     elif priority_score >= MEDIUM_PRIORITY_THRESHOLD:
-    #         return PriorityEnum.MEDIUM
-    #     else:
-    #         return PriorityEnum.LOW
-
-    # def _calculate_mold_progress(self, parts: List[Part]) -> float:
-    #     total_parts = len(parts)
-    #     completed_parts = self.__sum_parts_progress(parts)
-
-    #     if total_parts == 0:
-    #         return 0.0
-
-    #     return (completed_parts / total_parts) * 100
-
-    # @staticmethod
-    # def _sum_parts_progress(parts: List[Part]):
-    #     return sum(
-    #         1
-    #         if part.status == PartStatusEnum.COMPLETED
-    #         else 0.5
-    #         if part.status == PartStatusEnum.IN_PROGRESS
-    #         else 0.0
-    #         for part in parts
-    #     )
-
-    # @staticmethod
-    # def _update_mold_fields(payload: MoldBase, target: Mold) -> Mold:
-    #     for key, value in payload.model_dump(exclude_unset=True).items():
-    #         if hasattr(target, key):
-    #             setattr(target, key, value)
-    #     return target
-
+    
     def _validate_name_uniqueness(self, name: str, exclude_id: str = None) -> None:
         if self.mold_repo.get_mold_by_field('name', name, exclude_id=exclude_id):
             raise DataConflictError(f"A mold with name '{name}' already exists.")
+
+
+    def delete_mold(self, id: str) -> None:
+        mold = self._get_mold_or_404(id)
+        self.mold_repo.delete_mold(mold)
+
+    def update_mold(self, id: str, payload: MoldUpdatePayload) -> Mold:
+        if payload.progress_percentage is not None:
+            raise InvalidFieldError('Progress percentage cannot be changed')
+        if payload.priority is not None:
+            raise InvalidFieldError('Priority cannot be changed')
+
+        mold = self._get_mold_or_404(id)
+        updated_data = payload.model_dump(exclude_unset=True)
+
+        new_name = updated_data.get('name', mold.name)
+        self._validate_name_uniqueness(new_name, mold.id)
+        
+        its_a_new_delivery_date = True if 'delivery_date' in updated_data else False
+        
+        updated_mold = self._update_mold_fields(payload, mold)
+        
+        if its_a_new_delivery_date:
+            return self._calculate_priority(mold)
+        
+        return self.mold_repo.update_mold(updated_mold)
+
+    def get_mold(self, id: str) -> Mold:
+        mold = self._get_mold_or_404(id)
+        return self._calculate_priority(mold)
+
+    def _get_mold_or_404(self, id: str) -> Mold:
+        mold = self.mold_repo.get_mold_by_field('id', id)
+        if not mold:
+            raise NotFoundError('Mold not found')
+        return mold
+
+    def _calculate_priority(self, mold: Mold) -> Mold:
+        # if mold.priority_updated_at != date.today():
+        mold.priority_updated_at = date.today()
+        mold.progress_percentage = self.progress_service._calculate_mold_progress_percentage(mold.mold_parts)
+        mold.priority = self.progress_service.calculate_priority(
+            mold.delivery_date, mold.progress_percentage
+        )
+        mold = self.mold_repo.update_mold(mold)
+        return mold
+
+    @staticmethod
+    def _update_mold_fields(payload: MoldBase, target: Mold) -> Mold:
+        for key, value in payload.model_dump(exclude_unset=True).items():
+            if hasattr(target, key):
+                setattr(target, key, value)
+        return target
