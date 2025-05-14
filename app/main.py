d6fe23faa2927fb0952ccd3bc0aa76b1555a05ac
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-05-14 19:33:05 -0300

    feat: update allow_headers

--- app/main.py
+++ app/main.py
@@ -56,7 +56,7 @@ app.add_middleware(
     allow_origins=['*'],
     allow_credentials=True,
     allow_methods=['GET', 'POST', 'PATCH', 'PUT', 'DELETE', 'OPTIONS'],
-    allow_headers=['Authorization', 'Content-Type'],
+    allow_headers=["*"],
 )
 
 app.add_middleware(AuthenticationMiddleware)
