9e0f149fa81fefa90a339f64204261f0e7043590
YasminTrembulack <yasmin.agostinho.012@gmail.com>
2025-05-14 13:43:56 -0300

    fix: refresh_token route

--- app/routes/auth_route.py
+++ app/routes/auth_route.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
 
 from app.db.database import get_session
+from app.middlewares.check_roles import check_roles
 from app.services.auth_service import AuthService
 from app.types.schemas import LoginPayload, LoginResponse, RefreshTokenResponse, UserResponse
 
@@ -19,11 +20,17 @@ def login(user: LoginPayload, session: Session = Depends(get_session)):
         user=UserResponse.model_validate(_user.to_dict()),
     )
 
-@router.post('/refresh_token', status_code=status.HTTP_200_OK, response_model=RefreshTokenResponse)
-def refresh_token(refresh_token: str, session: Session = Depends(get_session)):
+@router.post(
+    '/refresh_token',
+    status_code=status.HTTP_200_OK,
+    response_model=RefreshTokenResponse
+)
+def refresh_token(
+    session: Session = Depends(get_session),
+    user: None = Depends(check_roles(['Admin', 'User', 'Editor']))
+):
     service = AuthService(session)
-    
-    new_access_token = service.refresh_token(refresh_token)
+    new_access_token = service.refresh_token(user.id)
 
     return RefreshTokenResponse(
         message='Access token refreshed successfully!',
